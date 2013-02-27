8f54850e51c7d3ff043e270ff057c37b3afa9e40
Brian Manning <brian@xaoc.org>
2013-02-27 15:39:14 -0800

    added routes for all, search and original index

--- App-Factoid-Browser/lib/App/Factoid/Browser.pm
+++ App-Factoid-Browser/lib/App/Factoid/Browser.pm
@@ -5,19 +5,41 @@ use Fcntl qw(/^O_/);
 
 our $VERSION = '0.1';
 
-get '/' => sub {
-    #template 'index';
+## setup
     my %factoids;
     my $filename = q(/srv/www/purl/html/Prospero/Prospero-is);
+    debug(qq(Reading in factoids from $filename));
     tie(%factoids, q(AnyDBM_File), $filename, O_RDONLY, 0644)
         || die "Couldn't open " .  $filename . " with AnyDBM_File: $!";
 
+### DEFAULT PAGE ###
+get q(/) => sub {
+    template 'index';
+};
+
+### DEFAULT PAGE ###
+get q(/original) => sub {
+    template 'index.orig';
+};
+
+### /all ###
+get q(/all) => sub {
+    #template 'index';
     foreach my $key(sort(keys(%factoids))) {
         print qq($key => ) . $factoids{$key} . qq(\n);
     }
-
-    untie(%factoids) || die "untie() on " . $filename . " failed: $!";
-
 };
 
+get q(/search/:query) => sub {
+    my $query = param(q(query));
+    debug(qq(/search/query; Query string is: $query));
+    my @found_keys = grep(/$query/, keys(%factoids));
+    debug(qq(/search/query; Found ) . scalar(@found_keys) . qq( keys));
+    foreach my $key ( @found_keys ) {
+        print qq($key => ) . $factoids{$key} . qq(\n);
+    }
+};
+
+# Exiting the script should close the filehandle
+# untie(%factoids) || die "untie() on " . $filename . " failed: $!";
 true;
