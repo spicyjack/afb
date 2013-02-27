e11056b15bbe75b2e7716f5152277ccdc0ae1396
Brian Manning <brian@xaoc.org>
2013-02-27 11:42:25 -0800

    added simple display of factoids

--- App-Factoid-Browser/lib/App/Factoid/Browser.pm
+++ App-Factoid-Browser/lib/App/Factoid/Browser.pm
@@ -1,10 +1,23 @@
 package App::Factoid::Browser;
-use Dancer ':syntax';
+use Dancer;
+use AnyDBM_File;
+use Fcntl qw(/^O_/);
 
 our $VERSION = '0.1';
 
 get '/' => sub {
-    template 'index';
+    #template 'index';
+    my %factoids;
+    my $filename = q(/srv/www/purl/html/Prospero/Prospero-is);
+    tie(%factoids, q(AnyDBM_File), $filename, O_RDONLY, 0644)
+        || die "Couldn't open " .  $filename . " with AnyDBM_File: $!";
+
+    foreach my $key(sort(keys(%factoids))) {
+        print qq($key => ) . $factoids{$key} . qq(\n);
+    }
+
+    untie(%factoids) || die "untie() on " . $filename . " failed: $!";
+
 };
 
 true;
