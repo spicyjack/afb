5f3ba01c0bccf2c80462d2547ad05905b5a93539
Brian Manning <brian@xaoc.org>
2013-03-01 15:18:23 -0800

    changed /all to /browse, set templates for all routes

--- App-Factoid-Browser/lib/App/Factoid/Browser.pm
+++ App-Factoid-Browser/lib/App/Factoid/Browser.pm
@@ -15,38 +15,59 @@ our $VERSION = '0.1';
 ### DEFAULT PAGE ###
 get q(/) => sub {
     set layout => q(miranda);
-    template 'index';
+    template q(index);
 };
 
-### DEFAULT PAGE ###
+### original Dancer page ###
 get q(/original) => sub {
     set layout => q(main);
-    template    q(index.orig);
+    template q(index.orig);
 };
 
-### /all ###
-get q(/all) => sub {
+### /browse ###
+get q(/browse) => sub {
     set layout => q(miranda);
+    template q(browse);
+
+=pod
+
     print qq(HTTP/1.1 200 OK\nContent-type: text/html\n\n);
     print qq(<html>\n<body>\n);
     foreach my $key(sort(keys(%factoids))) {
         print qq(<div>$key => ) . $factoids{$key} . qq(</div>\n);
     }
     print qq(</body>\n</html>\n);
+
+=cut
+
 };
 
+### /search page ###
 get q(/search/:query) => sub {
     set layout => q(miranda);
+    template q(search);
+};
+
+### /search with a :query ###
+get q(/search/:query) => sub {
+    set layout => q(miranda);
+    template q(search);
     my $query = param(q(query));
     debug(qq(/search/query; Query string is: $query));
     my @found_keys = grep(/$query/, keys(%factoids));
     debug(qq(/search/query; Found ) . scalar(@found_keys) . qq( keys));
+
+=pod
+
     print qq(HTTP/1.1 200 OK\nContent-type: text/html\n\n);
     print qq(<html>\n<body>\n);
     foreach my $key ( @found_keys ) {
         print qq(<div>$key => ) . $factoids{$key} . qq(</div>\n);
     }
     print qq(</body>\n</html>\n);
+
+=cut
+
 };
 
 # Exiting the script should close the filehandle
