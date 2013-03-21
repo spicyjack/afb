16027ffb2f860138f9078e5109f5bc1fa711b410
Brian Manning <brian@xaoc.org>
2013-03-20 17:37:46 -0700

    handle browser appending HTTP GET args to URI

--- App-Factoid-Browser/lib/App/Factoid/Browser.pm
+++ App-Factoid-Browser/lib/App/Factoid/Browser.pm
@@ -95,7 +95,20 @@ get q(/original) => sub {
 
 ### DEFAULT PAGE ###
 get q(/) => sub {
-    template q(search);
+    # not picking up :query here, hack around it
+    debug(q(plain /search: request_uri is ) . request->request_uri);
+    my $query = request->request_uri;
+    $query =~ s#\/\?search=##;
+    debug(qq(plain /search; Query string is: $query));
+    my @found_factoids = grep(/$query/, @combined_factoids);
+    debug(qq(plain /search; Found ) . scalar(@found_factoids) . qq( factoids));
+    my $data_ref = get_browse_data(
+        factoids => \@found_factoids,
+    );
+    template(q(search), {
+        factoids        => $data_ref,
+        search_string   => \$query,
+    });
 };
 
 ### /browse ###
@@ -182,17 +195,6 @@ get q(/search/:query/:start_num) => sub {
         search_string   => \$query,
     });
 };
-=pod
-
-    print qq(HTTP/1.1 200 OK\nContent-type: text/html\n\n);
-    print qq(<html>\n<body>\n);
-    foreach my $key ( @found_keys ) {
-        print qq(<div>$key => ) . $factoids{$key} . qq(</div>\n);
-    }
-    print qq(</body>\n</html>\n);
-
-=cut
-
 
 # Exiting the script should close the filehandle
 # untie(%factoids-is) || die "untie() on " . config->dbm_basename 
